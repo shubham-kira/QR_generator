4f10023290f459f6c323d51b35d2c15c3404f6f4
shubham-kira <shubham190shubham@gmail.com>
2019-02-23 01:04:56 +0530

    QR generator

---
+++ app/src/main/java/com/kira/shubham/qr/MainActivity.java
@@ -0,0 +1,68 @@
+package com.kira.shubham.qr;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.encoder.QRCode;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String BASE_URL = "https://deb4b7ed.ngrok.io/api/qr";
+    private int unique_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView qr_code = (ImageView) findViewById(R.id.qr_code);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest codeRequest = new StringRequest(Request.Method.GET, BASE_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //JSON parsing
+                try {
+                    JSONObject json = (JSONObject) new JSONObject(response);
+                    unique_id = json.getInt("id");
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode(String.valueOf(unique_id), BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        qr_code.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        requestQueue.add(codeRequest);
+    }
+}
