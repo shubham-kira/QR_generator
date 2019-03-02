fe876f5538e581f24f166c559a275d6371692c28
shubham-kira <shubham190shubham@gmail.com>
2019-03-02 22:58:36 +0530

    custom QR request

--- app/src/main/java/com/kira/shubham/qr/MainActivity.java
+++ app/src/main/java/com/kira/shubham/qr/MainActivity.java
@@ -1,8 +1,10 @@
 package com.kira.shubham.qr;
-
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.android.volley.Request;
@@ -23,15 +25,45 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String BASE_URL = "https://deb4b7ed.ngrok.io/api/qr";
+    private final String BASE_URL = "https://fa86cb46.ngrok.io/api/qr";
+    private String text;
     private int unique_id;
+    private EditText editText;
+    private Button button;
+    ImageView qr_code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView qr_code = (ImageView) findViewById(R.id.qr_code);
+        editText = (EditText) findViewById(R.id.custom_text);
+        button = (Button) findViewById(R.id.submit);
+        qr_code = (ImageView) findViewById(R.id.qr_code);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = editText.getText().toString();
+                if(text.length() == 0) {
+                    function();
+                } else {
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        qr_code.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+    }
+
+    void function() {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         StringRequest codeRequest = new StringRequest(Request.Method.GET, BASE_URL, new Response.Listener<String>() {
